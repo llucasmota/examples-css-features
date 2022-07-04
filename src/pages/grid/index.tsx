6302d30303990893f2729431f6d9cbbf303118e7
Lucas Mota <lucasmota@MacBook-Air-de-Lucas.local>
2022-07-04 07:38:51 -0300

    feat(*): added grid examples

---
+++ src/pages/grid/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { GridPageFactory } from '@main/factories/GridPage';
+
+const Grid: React.FC = () => {
+  return <GridPageFactory />;
+};
+
+export default Grid;
